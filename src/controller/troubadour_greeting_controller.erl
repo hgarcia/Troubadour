c73e011f4fc4622add3fe458149da7d330f0e5ac
Hernan Garcia <hernan@dynamicprogrammer.com>
2012-03-08 00:03:59 -0500

    Moving along with the tutorial.

--- src/controller/troubadour_greeting_controller.erl
+++ src/controller/troubadour_greeting_controller.erl
@@ -1,5 +1,21 @@
 -module(troubadour_greeting_controller, [Req]).
 -compile(export_all).
 
+list('GET', []) ->
+  Greetings = boss_db:find(greeting, []),
+  {ok, [{greetings, Greetings}]}.
+
+create('GET', []) ->
+  ok;
+create('POST', []) ->
+  GreetingText = Req:post_param("greeting_text"),
+  NewGreeting = greeting:new(id, GreetingText),
+  {ok, SavedGreeting} = NewGreeting:save(),
+  {redirect, [{action, "list"}]}.
+
+goodbye('POST', []) ->
+  boss_db:delete(Req:post_param("greeting_id")),
+  {redirect, [{action, "list"}]}.
+
 hello('GET', []) ->
-    {output, "<strong>Troubadour says hello!</strong>"}.
+  {ok, [{greeting, "Troubadour says hello!"}]}.
